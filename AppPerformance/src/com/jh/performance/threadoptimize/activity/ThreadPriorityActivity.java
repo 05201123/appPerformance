1d6b622e246eead794721d1aa4773452b78dcbdf
05201123 <1581022571@qq.com>
2016-05-12 13:55:28 +0800

    jh zk

---
+++ AppPerformance/src/com/jh/performance/threadoptimize/activity/ThreadPriorityActivity.java
@@ -0,0 +1,95 @@
+package com.jh.performance.threadoptimize.activity;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.jh.performance.utils.PerformanceUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+/**
+ * 线程与权限，数量
+ * @author 099
+ *
+ */
+public class ThreadPriorityActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		
+		Log.e("performance","cup num ="+PerformanceUtils.getNumCores());
+		
+		for(int i=0;i<10;i++){
+			Thread thread=new Thread(new MyRunnable2(i+5));
+			thread.start();
+		}
+		for(int i=0;i<10;i++){
+			Thread thread=new Thread(new MyRunnable(i));
+			thread.start();
+		}
+//		
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
+		executorService.execute(new MyRunnable(41));
+		executorService.execute(new MyRunnable2(42));
+		
+		long starttime=System.currentTimeMillis();
+		long startThreadTime=SystemClock.currentThreadTimeMillis();
+		fib(35);
+		long endtime=System.currentTimeMillis();
+		long endThreadTime=SystemClock.currentThreadTimeMillis();
+		Log.e("performance","mainThread"+"starttime= "+starttime+", endtime ="+endtime+", exectime ="+(endtime-starttime)
+				+"; startThreadTime ="+startThreadTime+", endThreadTime ="+endThreadTime+",execThreadtime ="
+				+(endThreadTime-startThreadTime));
+		
+	}
+	public static long fib(int n) {
+        if (n <= 1) return n;
+        else return fib(n-1) + fib(n-2);
+    }
+	 class MyRunnable implements Runnable{
+		
+		String myName;
+		MyRunnable(int i){
+			myName="subthread i="+i;
+		}
+		@Override
+		public void run() {
+			long starttime=System.currentTimeMillis();
+			long startThreadTime=SystemClock.currentThreadTimeMillis();
+			fib(35);
+			long endtime=System.currentTimeMillis();
+			long endThreadTime=SystemClock.currentThreadTimeMillis();
+			Log.e("performance",myName+"starttime= "+starttime+", endtime ="+endtime+", exectime ="+(endtime-starttime)
+					+"; startThreadTime ="+startThreadTime+", endThreadTime ="+endThreadTime+",execThreadtime ="
+					+(endThreadTime-startThreadTime));
+			
+		}
+		
+	}
+	 class MyRunnable2 implements Runnable{
+			
+			String myName;
+			MyRunnable2(int i){
+				myName="subthread i="+i;
+			}
+			@Override
+			public void run() {
+				android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
+				long starttime=System.currentTimeMillis();
+				long startThreadTime=SystemClock.currentThreadTimeMillis();
+				fib(35);
+				long endtime=System.currentTimeMillis();
+				long endThreadTime=SystemClock.currentThreadTimeMillis();
+				Log.e("performance",myName+"starttime= "+starttime+", endtime ="+endtime+", exectime ="+(endtime-starttime)
+						+"; startThreadTime ="+startThreadTime+", endThreadTime ="+endThreadTime+",execThreadtime ="
+						+(endThreadTime-startThreadTime));
+				
+			}
+			
+		}
+	
+	
+}
